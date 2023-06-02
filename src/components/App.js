bd2cbaf75d653dfdee3a67ba40484c09ff7b8a04
abdurrahman6489 <120268868+abdurrahman6489@users.noreply.github.com>
2023-06-03 00:00:29 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -6,25 +6,29 @@ const App = () => {
   const [isLoading, setIsLoading] = useState(false);
   const [photo, setPhoto] = useState({});
   const BASE_URL = "https://jsonplaceholder.typicode.com/photos/";
-  async function fetchApi(id){
+  async function fetchApi(id) {
+    if (id == 0) {
+      setPhoto({});
+      return;
+    }
     setIsLoading(true);
-    const response = await fetch(BASE_URL+id);
+    const response = await fetch(BASE_URL + id);
     const data = await response.json();
     setIsLoading(false);
     setPhoto(data);
   }
-  function handleChange(event){
+  function handleChange(event) {
     let value = event.target.value;
     fetchApi(value);
   }
-  
+
   return (
-  <div id="main">
-    <label htmlFor="input">Id number</label>
-    <input id="input" type="number" onChange={handleChange}/>
-    {isLoading && <Loader />}
-    {photo.id > 0 && <PhotoFrame {...photo} />}
-  </div>
-  )
-}
+    <div id="main">
+      <label htmlFor="input">Id number</label>
+      <input id="input" type="number" onChange={handleChange} />
+      {isLoading && <Loader />}
+      {photo.id > 0 && <PhotoFrame {...photo} />}
+    </div>
+  );
+};
 export default App;
